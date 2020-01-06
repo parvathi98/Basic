a97ca285c0bf5bec74d3113d6abcc7705bfecdeb
parvathi98 <52947332+parvathi98@users.noreply.github.com>
2020-01-06 21:41:58 +0530

    Create 4.py

---
+++ program/4.py
@@ -0,0 +1,6 @@
+length = float(input('Please Enter the Length of a Triangle: '))
+width = float(input('Please Enter the Width of a Triangle: '))
+
+area = length * width
+
+print("The Area of a Rectangle using", length, "and", width, " = ", area)
