2674311fb712cd0afbe64221c3156394738eecfd
parvathi98 <52947332+parvathi98@users.noreply.github.com>
2020-01-06 22:53:48 +0530

    Create 11.py

---
+++ program/11.py
@@ -0,0 +1,6 @@
+a = float(input(' Enter the First Angle of a Triangle: '))
+b = float(input(' Enter the Second Angle of a Triangle: '))
+
+c = 180 - (a + b)
+
+print("Third Angle of a Triangle = ", c)
