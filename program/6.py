e18d5e48274b3ef5411e7d4adbb56fea7a1ef522
parvathi98 <52947332+parvathi98@users.noreply.github.com>
2020-01-06 22:01:38 +0530

    Create 6.py

---
+++ program/6.py
@@ -0,0 +1,8 @@
+cm = 1000;  
+meter = cm / 100.0; 
+kilometer = cm / 100000.0; 
+print("Length in meter = " , 
+               meter , "m"); 
+print("Length in Kilometer = ",  
+             kilometer , "km"); 
+  
